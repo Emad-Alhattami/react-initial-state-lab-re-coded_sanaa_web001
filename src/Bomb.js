d730378a02a2cbe5ac609a94af07f83b3f44b589
Emad Alhattami <emad774373827@gmail.com>
2020-08-11 22:30:17 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,7 +1,7 @@
 // your Bomb code here!import React from 'react';
 class Bomb extends React.Component {
   constructor(props) {
-    super() 
+    super()
 
     this.state = {
       secondsLeft: props.initialCount
@@ -23,7 +23,7 @@ import React from 'react';
 class Bomb extends React.Component {
 
   constructor(props) {
-    super() 
+    super()
 
     this.state = {
       secondsLeft: props.initialCount
